1d6725fac0d96d539fa4457bbd306cdb4a8df9aa
Matt Eves <mattheweves@outlook.com>
2017-05-19 16:03:30 -0700

    Create Tests

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe NotesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "notes#create action" do
+    before do
+      post :create, params: {note: {title: 'First', content: 'Hello'}}
+    end
+    it "should return 200 status-code" do
+      expect(response).to be_success
+      #expect(response).to have_http_status(:success) This is the same as above
+    end
+
+    it "should successfully create & save a note in the db" do
+      note = Note.last
+      expect(note.content).to eq('Hello')
+      expect(note.title).to eq('First')
+    end
+
+    it "should return the created note in the response body" do
+      json = JSON.parse(response.body)
+      expect(json['content']).to eq('Hello')
+      expect(json['title']).to eq('First')
+    end
+  end
 end
